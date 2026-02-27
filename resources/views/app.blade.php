47a33d2b01351a389d411e55c95f737a3b664062
DarthLakren <167852236+DarthLakren@users.noreply.github.com>
2026-02-26 22:07:58 -0800

    Welcomo pagina
    Todo lo de la act 5 xd

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi App</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+
+    <h1 class="text-center">Hola mundo</h1>
+
+</body>
+</html>
